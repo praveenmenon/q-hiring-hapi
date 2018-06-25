2d047bf6e26a4df9de3387346b9d396c6b6d505d
vsanjeevagowda-s <vsanjeevagowda@qwinix.io>
2018-06-25 06:04:07 +0530

    abstraction url fixes for environments

--- abstractionUrl.js
+++ abstractionUrl.js
@@ -1,2 +1,6 @@
-const abstractionRootUrl =  "q-hiring-abstraction:3002";
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') {
+  const abstractionRootUrl = "q-hiring-abstraction:3002";
+} else {
+  const abstractionRootUrl = "http://localhost:3002/";
+}
 module.exports = abstractionRootUrl;
