d75db3b19752ca8e8be5f5ae190ca70b37bff5e0
vsanjeevagowda-s <vsanjeevagowda@qwinix.io>
2018-06-25 06:18:28 +0530

    fixe

--- abstractionUrl.js
+++
@@ -1,6 +0,0 @@
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') {
-  const abstractionRootUrl = "q-hiring-abstraction:3002";
-} else {
-  const abstractionRootUrl = "http://localhost:3002/";
-}
-module.exports = abstractionRootUrl;
